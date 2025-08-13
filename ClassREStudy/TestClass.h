523360f792f1074b9159c8a8373650825a502acd
RTLCopyMemory <info@protocli.me>
2025-08-13 17:09:03 +0200

    Full inlining of functions

--- ClassREStudy/TestClass.h
+++ ClassREStudy/TestClass.h
@@ -1,37 +1,36 @@
 #pragma once
-
 #include "VirtualBase.h"
 
-class TestClass: VirtualBase
+class TestClass : VirtualBase
 {
 public:
-	TestClass();
-	~TestClass();
+    __declspec(noinline) TestClass();
+    __declspec(noinline) ~TestClass();
 
-	int getTestValue() const;
-	void setTestValue(int value);
+    __declspec(noinline) int getTestValue() const;
+    __declspec(noinline) void setTestValue(int value);
 
-	static void staticFunction();
-	static int getStaticValue();
-	static void setStaticValue(int value);
+    __declspec(noinline) void virtualFunction();
+    __declspec(noinline) void pureVirtualFunction();
 
-	virtual void virtualFunction(); // virtual function
-	void pureVirtualFunction();
+    __declspec(noinline) static void staticFunction();
+    __declspec(noinline) static int getStaticValue();
+    __declspec(noinline) static void setStaticValue(int value);
 
-	int handleNumber(int number);
-	float handleNumber(float number);
-	double handleNumber(double number);
-	short handleNumber(short number);
+    __declspec(noinline) int handleNumber(int number);
+    __declspec(noinline) float handleNumber(float number);
+    __declspec(noinline) double handleNumber(double number);
+    __declspec(noinline) short handleNumber(short number);
 
-	template<typename T>
-	T handleNumberTemplate(T number) {
-	    volatile unsigned char marker[] = { 0xA7, 0xB8, 0xC9, 0xDA, 0xEB, 0xFC };
-	    (void)marker;
-	    return number;
-	}
+    template<typename T>
+    __declspec(noinline) T handleNumberTemplate(T number) {
+        volatile unsigned char marker[] = { 0xA7, 0xB8, 0xC9, 0xDA, 0xEB, 0xFC };
+        (void)marker;
+        return number;
+    }
 
 private:
-	int testValue;
-	static int staticValue;
+    int testValue;
+    static int staticValue;
 };
 
